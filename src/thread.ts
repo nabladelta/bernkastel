8450f921014f5ed80f66425ba7d45114bc223c3f
reasv <7143787+reasv@users.noreply.github.com>
2019-10-16 12:13:02 +0200

    Add LogEntry interface

--- src/thread.ts
+++ src/thread.ts
@@ -1,18 +1,34 @@
 import IPFS from "ipfs";
 import OrbitDB from "orbit-db"
+import { FeedStore } from "orbit-db-feedstore";
+import { Identity } from "orbit-db-identity-provider";
+interface LogEntry {
+        hash: string,
+        id: string,
+        payload: { op: string, key?: string, value: object},
+        next: object[],
+        v: 1,
+        clock: object,
+        key: string,
+        identity: Identity,
+        sig: string
+}
 export class Thread {
     ipfs: ipfs
     orbit: OrbitDB
     address: string
+    db: FeedStore<LogEntry>
     ready: Promise<OrbitDB>
     constructor(ipfs: ipfs, orbit: OrbitDB, address?: string){
         this.ipfs = ipfs
         this.orbit = orbit
-        if (address == undefined){
-            const db = this.orbit.create(Date.now().toString(), 'feed')
-        } else {
-            this.address = address
-            this.orbit.open(this.address)
-        }
+        this.ready = new Promise((resolve, reject) => {
+            if (address == undefined){
+                const db = this.orbit.create(Date.now().toString(), 'feed')
+            } else {
+                this.address = address
+                this.orbit.open(this.address)
+            }
+        })
     }
 }
