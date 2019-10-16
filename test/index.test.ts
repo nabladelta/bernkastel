5868a20f55bd55c19e4a6d77ce797fcf33363bce
reasv <7143787+reasv@users.noreply.github.com>
2019-10-16 11:57:21 +0200

    Finish IPFS & OrbitDB startup test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
-import {initIPFS, Thread} from "../src/index"
+import {initIPFS} from "../src/index"
+import OrbitDB from "orbit-db";
 import { doesNotReject } from 'assert';
-
-describe('IPFS', function() {
-    it('starts IPFS', async () => {
+import { FeedStore } from 'orbit-db-feedstore';
+describe('IPFS & OrbitDB', function() {
+    it('starts IPFS and OrbitDB', async () => {
       const now = Date.now()
       const ipfsOptions = {
         repo: `./orbitdb/${now}/ipfs`,
@@ -21,5 +22,12 @@ describe('IPFS', function() {
         }
       }
       const node = await initIPFS(ipfsOptions)
-    }); 
-  });
+      const peers = await node.swarm.peers()
+      expect(peers.length).gt(0)
+      const orbit = await OrbitDB.createInstance(node, {directory: `./orbitdb/${now}/orbitdb`})
+      const db = (await orbit.create(now.toString(), 'feed')) as FeedStore<{payload:{value:object}}>
+      const h = await db.add(now.toString())
+      const v = await db.get(h)
+      expect(v.payload.value).eq(now.toString())
+    })
+  })
