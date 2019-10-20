a72a577e77d48c91461aede23e8d700fff967dbe
reasv <7143787+reasv@users.noreply.github.com>
2019-10-20 17:42:26 +0200

    Implement ThreadAccessController

--- src/ThreadAccessController/ThreadAccessController.ts
+++ src/ThreadAccessController/ThreadAccessController.ts
@@ -1,5 +1,62 @@
 import OrbitDBAccessController from 'orbit-db-access-controllers/src/orbitdb-access-controller'
+import { Post } from '../Post'
+import OrbitDB from 'orbit-db'
+import Identities from 'orbit-db-identity-provider'
+const pMapSeries = require('p-map-series')
+function sizeOf(obj: any) {
+    var bytes = 0
+    if(obj !== null && obj !== undefined) {
+        switch(typeof obj) {
+        case 'number':
+            bytes += 8;
+            break;
+        case 'string':
+            bytes += obj.length * 2;
+            break;
+        case 'boolean':
+            bytes += 4;
+            break;
+        case 'object':
+            var objClass = Object.prototype.toString.call(obj).slice(8, -1);
+            if(objClass === 'Object' || objClass === 'Array') {
+                for(var key in obj) {
+                    if(!obj.hasOwnProperty(key)) continue;
+                    sizeOf(obj[key]);
+                }
+            } else bytes += obj.toString().length * 2;
+            break;
+        }
+    }
+    return bytes
+}
 export default class ThreadAccessController extends OrbitDBAccessController{
     static get type () { return 'thread' }
+    get type () {return 'thread'}
+    constructor (orbitdb: OrbitDB, options: any) {
+        super(orbitdb, options)
+      }
+    async canAppend(entry: LogEntry<Post>, identityProvider: Identities){
+        if (!super.canAppend(entry, identityProvider)) return false
+        const post = entry.payload.value
+        if (!post.time) return false
+        if (post.message && post.message.length > 4000) return false
+        if (post.title && post.title.length > 128) return false
+        if (post.author && post.author.length > 128) return false
+        if (post.quote && post.quote.length > 64) return false
+        if (post.attachments && post.attachments.length > 64) return false
+        if (post.delete && post.delete.length > 64) return false
+        if (post.deletedBy || post.own || post.hide) return false // forbidden properties
+        return true
+    }
+    static async create (orbitdb: OrbitDB, options: any) {
+        const ac = new ThreadAccessController(orbitdb, options)
+        await (ac as OrbitDBAccessController).load(options.address || options.name || 'default-access-controller')
+        // Add write access from options
+        if (options.write && !options.address) {
+          await pMapSeries(options.write, async (e) => (ac as OrbitDBAccessController).grant('write', e))
+        }
+    
+        return ac
+    }
 
 }
