cc7fea884b4464c6fb6a9fdfb88900c03840b53b
reasv <7143787+reasv@users.noreply.github.com>
2019-10-20 16:20:33 +0200

    Improve Thread Identity system

--- src/Thread.ts
+++ src/Thread.ts
@@ -3,6 +3,7 @@ import OrbitDB from "orbit-db"
 import ThreadStore from './ThreadStore'
 import {Post} from './Post'
 import Identities, { Identity } from "orbit-db-identity-provider";
+import { Keystore } from "orbit-db-keystore";
 interface IThreadOptions {
     moderators?: Set<string>
     identity?: Identity
@@ -16,29 +17,42 @@ class Thread {
     public moderators: Set<string> // ids of moderators
     public ownPosts: Set<string> // hashes of own posts
     public anonymous: boolean
-
-    constructor(ipfs: IPFS, orbit: OrbitDB, address?: string, options?: IThreadOptions){
+    _anonymousKeystore: Keystore
+    
+    constructor({ ipfs, orbit, address, moderators = new Set<string>(), anonymous = false, identity } : 
+                {ipfs: IPFS, orbit: OrbitDB, address?: string, moderators?: Set<string>, anonymous?: boolean, identity?: Identity}){
         this.ipfs = ipfs
         this.orbit = orbit
-        this.ownPosts = new Set<string>([])
+        this.ownPosts = new Set<string>()
+        this.moderators = moderators
+        this.anonymous = anonymous
         this.ready = new Promise(async (resolve, reject) => {
             if (address == undefined){
-                this.db = await this.orbit.create(Date.now().toString(), 'thread', {write: ["*"]}) as ThreadStore
+                this.db = await this.orbit.create(Date.now().toString(), 'thread', {accessController: {write: ["*"]}}) as ThreadStore
             } else {
                 this.db = await this.orbit.open(address) as ThreadStore
             }
-            if (options) this.setOptions(options)
-            this.db.events.on('replicated', this.replicated)
+            if (identity) this.identity = identity
+            if (anonymous) {
+                this.identity = await Identities.createIdentity({identityKeysPath: `./orbitdb/anonymous/${Date.now()}`, id:`${Date.now()}`})
+                this._anonymousKeystore = this.identity.provider.keystore
+            }
+            this.bindOnReplicated(this.replicated)
             resolve()
         })
     }
-    setOptions(options: IThreadOptions){
-        if (options.moderators) this.moderators = options.moderators
-        if (options.identity) this.db.setIdentity(options.identity)
-        if (options.anonymous) this.anonymous = options.anonymous
+    get identity (){
+        return this.db ? this.db.identity : undefined
+    }
+    set identity (identity: Identity) {
+        this.db.setIdentity(identity)
     }
     get posts(){
         return this.db.all.map((entry) => {
+            if (this.ownPosts.has(entry.hash)){ // mark own posts as such
+                entry.payload.value.own = true
+            }
+            delete entry.payload.value.hide
             if (entry.payload.value.deletedBy == undefined) return entry // no one has deleted this entry
 
             if (entry.payload.value.deletedBy.has(entry.identity.id)){ // deleted by the original creator
@@ -46,8 +60,8 @@ class Thread {
                 return entry
             }
             
-            const intersect = [...this.moderators].filter(id => entry.payload.value.deletedBy.has(id)) // intersect moderator set and deletedBy
-            if (intersect.length > 0){ // if there is an intersection, one of our moderators has deleted this post
+            const intersect = [...Array.from(this.moderators)].filter(id => entry.payload.value.deletedBy.has(id)) // intersect moderator set and deletedBy
+            if (intersect.length > 0) { // if there is an intersection, one of our moderators has deleted this post
                 entry.payload.value.hide = true
                 return entry
             }
@@ -56,8 +70,8 @@ class Thread {
     }
     async post(post: Post){
         if (this.anonymous){
-            const identity = await Identities.createIdentity({identityKeysPath: `./orbitdb/keys/${Date.now()}`})
-            this.db.setIdentity(identity)
+            const identity = await Identities.createIdentity({id:`${Date.now()}`, keystore: this._anonymousKeystore})
+            this.identity = identity
         }
         const hash = await this.db.add(post)
         this.ownPosts.add(hash)
@@ -69,19 +83,11 @@ class Thread {
     async undeletePost(hash: string){
         return await this.db.undel(hash) // returns hash of undelete operation
     }
-    read(options?: {
-        gt?: string;
-        gte?: string;
-        lt?: string;
-        lte?: string;
-        limit?: number;
-        reverse?: boolean;
-    }){
-        const posts = this.db.iterator(options).collect()
-        return posts
-    }
     async replicated(_address: string){ // fired whenever we receive new posts from other peers
         //TODO: find, handle new entries
+    }
+    bindOnReplicated(fn: (a: string) => void){ // bind a function to the replicate event
+        this.db.events.on('replicated', fn)
     }
 }
 export = Thread
