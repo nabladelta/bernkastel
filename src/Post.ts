65e3130f571cacd20050c14c088cc6cb0e3aa360
reasv <7143787+reasv@users.noreply.github.com>
2019-10-20 16:17:04 +0200

    Add own property to Post

--- src/Post.ts
+++ src/Post.ts
@@ -5,8 +5,9 @@ export interface Post {
     attachments?: string[],
     title?: string,
     time: number
+    delete?: string // hash of post to delete, include if deleting a post
     // properties that are added by the system. Cannot be set by creator of post.
     deletedBy?: Set<string> // set of public keys that want this deleted
-    delete?: string // hash of post to delete, include if deleting a post
     hide?: true
+    own?: true // post made by yourself
 }
