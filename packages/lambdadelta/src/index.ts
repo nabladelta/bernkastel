ff07f9d8d650612923f96ef70de247c08fef238c
reilgun <87247753+reilgun@users.noreply.github.com>
2023-06-27 10:59:17 +0200

    Remove separate libraries

--- packages/lambdadelta/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Lambdadelta, NullifierSpec } from "./lambdadelta"
-import { generateMemberCID, verifyMemberCIDProof } from "./membercid"
-import { LDNode, LDNodeBase } from "./node"
-import { Timeline } from "./timeline"
-export { Lambdadelta, generateMemberCID, verifyMemberCIDProof, LDNode, LDNodeBase, NullifierSpec, Timeline }
